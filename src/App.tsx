33d48955f7536cb6a688f2e421d74de4b0585237
anwar <anwaralanzo6@gmail.com>
2022-12-12 00:38:51 +0300

    adding the first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import * as React from "react"
+import './App.css'
 import {
+  Button,
   ChakraProvider,
+  Flex,
+  Stack,
   theme,
+  VStack,
 } from "@chakra-ui/react"
 import Router1 from "./components/Router1"
 import {Routes,Route,BrowserRouter}from 'react-router-dom';
-import LoginForm from "./components/Forms/LoginForm";
+import FIRST from "./Pages/FIRST";
 
 
 
 export const App = () => (
   <BrowserRouter>
+
+<FIRST></FIRST>
   <ChakraProvider theme={theme}>
 
-<Router1/>
+{/* <Router1/> */}
   </ChakraProvider>
   </BrowserRouter>
 )
--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -60,7 +60,8 @@ import Navbar from '../components/navbar/Navbar';
   
     return (
 
-      <><Navbar links={[{ link: "/", alias: "About" }, { link: "/", alias: "Experience" }, { link: "/", alias: "CREATE ACCOUNT" }, { link: "/", alias: "SIGN IN" },]} logo={'https://media.discordapp.net/attachments/1036228185756541008/1051215907659190422/logo_transparent.png?width=936&height=936'} /><Flex justifyContent='center' alignItems='center' height='100vh'>
+      <><Navbar links={[{ link: "/", alias: "About" }, { link: "/", alias: "Experience" }, { link: "/", alias: "CREATE ACCOUNT" }, { link: "/", alias: "SIGN IN" },]} logo={'https://media.discordapp.net/attachments/1036228185756541008/1051215907659190422/logo_transparent.png?width=936&height=936'} />
+      <Flex justifyContent='center' alignItems='center' height='100vh'>
         <VStack spacing='2rem' width='20rem'>
           <Heading>Login </Heading>
           <LoginForm
