7c07dc99d66c0bf890655cdf9b0496b109943e3c
majed <m.a.a.almaged@gmail.com>
2022-12-12 23:02:31 +0300

    complate nav Bar

--- src/components/Router1.tsx
+++ src/components/Router1.tsx
@@ -7,6 +7,8 @@ import { Box } from '@chakra-ui/react';
 import Footer from '../components/Footer';
 import WelcomePage from '../Pages/WelcomePage';
 import Navbar from './navbar/Navbar';
+import Navbar1 from './navbar/Navbar';
+
 import Logout from './Logout';
 
 
@@ -16,20 +18,31 @@ function Router1() {
   const navigate = useNavigate();
 
   const logout1 = () => {
+    if(!localStorage.getItem("token")){
 
-    localStorage.removeItem('token');
-    navigate('/login');
+    return  <Navbar links={[  { link:"/login" , alias:"LOG IN" }, { link: "/register", alias: "REGISTER" },]} logo={'https://media.discordapp.net/attachments/1036228185756541008/1051215907659190422/logo_transparent.png?width=936&height=936'} />
+    }else{
+      return <Navbar1 links={[ { link: "/login", alias: "LOG OUT" },]} logo={'https://media.discordapp.net/attachments/1036228185756541008/1051215907659190422/logo_transparent.png?width=936&height=936'} />
+
+    }
+
+    // localStorage.removeItem('token');
     
+
   };
 
 
   return (
-    
-<Box
-// bg={"#2b2b2b"}
->        <Navbar links={[  { link:!localStorage.getItem("token")?"/login":"/login" , alias: "REGISTER" }, { link: "/login", alias: "LOG IN" },]} logo={'https://media.discordapp.net/attachments/1036228185756541008/1051215907659190422/logo_transparent.png?width=936&height=936'} />
+   
 
- 
+<Box
+
+// bg={"#2b2b2b"}
+>   
+{logout1()}
+
+
+
 <Routes>
 
 
